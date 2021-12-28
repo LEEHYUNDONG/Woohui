800799e6a4916c9c5a80495da65e0cb66e0908e2
LEEHYUNDONG <l.steve10@yahoo.com>
2021-12-28 15:51:07 +0900

    feat :search engine/2021/12/28

---
+++ script.js
@@ -0,0 +1,28 @@
+const Searching=Search.prototype;
+
+function Search() {
+    this.keword=document.querySelector('input[name= "search"]')
+    this.engine=document.querySelector('.SelectSearch');
+    this.button=document.querySelector('.img-button');
+    this.form=document.querySelector('.search');
+
+    this.Engine();
+};
+
+
+
+Searching.Engine=function () {
+    this.form.addEventListener('submit', e => {
+        e.preventDefault();
+
+        let engine=this.engine.value;
+        let keyword=this.keword.value;
+
+        if(engine==='google') {
+            location.href='https://www.google.co.kr/search?q='+keyword;
+        } else if(engine==='naver') {
+            location.href='https://search.naver.com/search.naver?query='+keyword;
+        }
+    });
+}
+new Search();
