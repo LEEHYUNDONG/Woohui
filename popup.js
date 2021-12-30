4bc5009f22ea8cff45941e95202fb19e7781e1a7
LEEHYUNDONG <l.steve10@yahoo.com>
2021-12-30 17:54:15 +0900

    body : popup.js를 수정하여 html이 동작할 때 새로운 탭에서 사이트가 열리도록 함.

--- popup.js
+++ popup.js
@@ -1,1 +1,23 @@
-alert("hello!");
+document.getElementById("myButton").addEventListener("click", Searching);
+
+function Searching() {
+    var engine=document.querySelector('.SelectSearch').value;
+    var text=document.querySelector('input[name= "search"]').value;
+
+    if(text!=null) {
+        var urls;
+        if(engine==="google") {
+            urls='https://www.google.co.kr/search?q=';
+        } else if(engine==="naver") {
+            urls='https://search.naver.com/search.naver?query=';
+        } else if(engine==="github") {
+            urls="https://github.com/search?q=";
+        } else if(engine==="stackoverflow") {
+            urls="https://stackoverflow.com/search?q=";
+        } else if(engine==="velog") {
+            urls="https://velog.io/search?q=";
+        }
+        urls+=text;
+        chrome.tabs.create({url: urls});
+    }
+}
